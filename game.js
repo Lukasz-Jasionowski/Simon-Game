b3525c4087cc89ae02833e3b2b64d053d9b025b6
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-02-12 16:52:48 +0100

    Adding game start function & click compatibility

--- game.js
+++ game.js
@@ -1,28 +1,61 @@
 var buttonColours = ["red", "blue", "green", "yellow"]
-var gamePatern = [];
+var gamePattern = [];
 var userClickedPattern = [];
-$('.btn').click(function(){
-    var userChosenColour = $(this).attr('id'); 
+var started = false;
+var level = 0;
+
+$('.btn').click(function () {
+    var userChosenColour = $(this).attr('id');
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
 });
+
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
-    gamePatern.push(randomChosenColour);
+    gamePattern.push(randomChosenColour);
     $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
 }
-function playSound(name){
+
+function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
-function animatePress(currentColor){
+
+function animatePress(currentColor) {
     $('#' + currentColor).addClass('pressed');
-    setTimeout(function (){
+    setTimeout(function () {
         $('#' + currentColor).removeClass('pressed');
     }, 100);
+}
+
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+    else {
+    }
+});
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        console.log("wrong");
+    }
 }
